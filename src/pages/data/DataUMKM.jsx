b187c5ea22ea81ed0ea65cd71fe208a4dc65fc1f
Rifannvl <riffanafendi@gmail.com>
2025-05-14 00:48:31 +0700

    api statis

---
+++ src/pages/data/DataUMKM.jsx
@@ -0,0 +1,199 @@
+// src/data/dataUMKM.js
+import logo from "../../assets/logo.svg";
+
+const dataUMKM = {
+  tentangKami: {
+    nama: "UMKM Sejahtera",
+    deskripsi:
+      "UMKM Sejahtera adalah usaha mikro, kecil, dan menengah yang bergerak di bidang kerajinan tangan dan produk lokal. Kami berdedikasi untuk memberikan produk berkualitas tinggi dengan harga terjangkau, serta mendukung perekonomian lokal melalui pemberdayaan masyarakat sekitar.",
+    logoUrl: logo,
+  },
+  produkUnggulan: [
+    {
+      id: 1,
+      nama: "Kerajinan Tangan A",
+      deskripsi:
+        "Deskripsi singkat produk kerajinan tangan dengan bahan alami yang ramah lingkungan.",
+      harga: 20000,
+      gambar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp_6CDS1bc5TkrT3_3vR5_iwPkp5Z9mrdRVw&s",
+    },
+    {
+      id: 2,
+      nama: "Produk Makanan B",
+      deskripsi:
+        "Makanan khas daerah dengan cita rasa otentik, cocok untuk oleh-oleh.",
+      harga: 40000,
+      gambar:
+        "https://img.freepik.com/premium-photo/close-up-bakpia-served-wooden-bowl-isolated-white-background_992019-2325.jpg?ga=GA1.1.474091255.1747151520&semt=ais_hybrid&w=740",
+    },
+    {
+      id: 3,
+      nama: "Aksesoris C",
+      deskripsi:
+        "Aksesoris handmade dengan desain modern dan elegan untuk segala usia.",
+      harga: 30000,
+      gambar:
+        "https://img.freepik.com/free-photo/jewellery-bangle-background-with-place-text-banner-fashion-accessories_460848-13242.jpg?ga=GA1.1.474091255.1747151520&semt=ais_hybrid&w=740",
+    },
+    {
+      id: 4,
+      nama: "Kerajinan Kayu D",
+      deskripsi: "Produk dari kayu jati dengan ukiran tradisional.",
+      harga: 75000,
+      gambar:
+        "https://img.freepik.com/free-photo/wooden-clock-view_1112-395.jpg?ga=GA1.1.474091255.1747151520&semt=ais_hybrid&w=740",
+    },
+    {
+      id: 5,
+      nama: "Makanan Ringan E",
+      deskripsi: "Keripik singkong gurih dan renyah.",
+      harga: 15000,
+      gambar:
+        "https://img.freepik.com/free-photo/salted-slice-lunch-unhealthy-potato_1339-45.jpg?ga=GA1.1.474091255.1747151520&semt=ais_hybrid&w=740",
+    },
+    {
+      id: 6,
+      nama: "Tas Rajut F",
+      deskripsi: "Tas cantik hasil rajutan tangan.",
+      harga: 50000,
+      gambar:
+        "https://img.freepik.com/free-photo/close-up-kitted-bag-still-life_23-2150709525.jpg?ga=GA1.1.474091255.1747151520&semt=ais_hybrid&w=740",
+    },
+    {
+      id: 7,
+      nama: "Sabun Herbal G",
+      deskripsi: "Sabun alami dari bahan herbal lokal.",
+      harga: 25000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2016/11/29/03/20/soap-1861087_960_720.jpg",
+    },
+    {
+      id: 8,
+      nama: "Dompet Kulit H",
+      deskripsi: "Dompet kulit asli buatan tangan.",
+      harga: 65000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2016/03/27/20/57/wallet-1281235_960_720.jpg",
+    },
+    {
+      id: 9,
+      nama: "Kopi Lokal I",
+      deskripsi: "Biji kopi pilihan dari petani lokal.",
+      harga: 30000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2015/09/02/12/35/coffee-918926_960_720.jpg",
+    },
+    {
+      id: 10,
+      nama: "Batik Cap J",
+      deskripsi: "Kain batik cap dengan motif khas Jawa.",
+      harga: 90000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2020/06/21/17/51/batik-5325281_960_720.jpg",
+    },
+    {
+      id: 11,
+      nama: "Topi Anyaman K",
+      deskripsi: "Topi dari daun pandan, cocok untuk wisata.",
+      harga: 22000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2014/11/07/21/39/hat-521302_960_720.jpg",
+    },
+    {
+      id: 12,
+      nama: "Gelang Kayu L",
+      deskripsi: "Aksesoris gelang dari kayu kelapa.",
+      harga: 18000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2020/01/28/06/29/bracelet-4798763_960_720.jpg",
+    },
+    {
+      id: 13,
+      nama: "Teh Herbal M",
+      deskripsi: "Campuran daun teh dan rempah untuk kesehatan.",
+      harga: 28000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2016/07/19/09/47/tea-1528889_960_720.jpg",
+    },
+    {
+      id: 14,
+      nama: "Lilin Aromaterapi N",
+      deskripsi: "Lilin wangi handmade untuk relaksasi.",
+      harga: 35000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2015/07/02/10/28/candle-828350_960_720.jpg",
+    },
+    {
+      id: 15,
+      nama: "Syal Tenun O",
+      deskripsi: "Syal tenun tradisional yang lembut dan hangat.",
+      harga: 48000,
+      gambar:
+        "https://cdn.pixabay.com/photo/2017/01/07/10/36/wool-1964286_960_720.jpg",
+    },
+  ],
+  //   callToAction
+  callToAction: {
+    judul: "Bergabunglah dengan kami dalam mendukung UMKM lokal!",
+    deskripsi:
+      "Belanja sekarang dan rasakan manfaatnya untuk perekonomian lokal!",
+    tombolText: "Kunjungi Toko Kami",
+    tombolUrl: "#",
+  },
+  //   testimoni
+  testimoni: [
+    {
+      id: 1,
+      nama: "Andi",
+      komentar: "Produknya sangat berkualitas dan pengirimannya cepat!",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+    },
+    {
+      id: 2,
+      nama: "Siti",
+      komentar: "Saya sangat puas dengan pelayanan dan kualitas barangnya.",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
+    },
+    {
+      id: 3,
+      nama: "Budi",
+      komentar: "UMKM lokal ini patut didukung! Produknya keren dan otentik.",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png",
+    },
+    {
+      id: 4,
+      nama: "Dewi",
+      komentar: "Desain produk unik dan harga terjangkau. Recommended banget!",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/3/34/PICA.jpg",
+    },
+    {
+      id: 5,
+      nama: "Rudi",
+      komentar: "Respon seller cepat, barang sampai dalam kondisi aman.",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+    },
+    {
+      id: 6,
+      nama: "Lina",
+      komentar: "Sangat suka dengan detail produknya. Akan order lagi.",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+    },
+    {
+      id: 7,
+      nama: "Agus",
+      komentar: "Berkualitas dan sesuai ekspektasi. Layanan ramah juga.",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
+    },
+  ],
+
+  //contact
+  kontak: {
+    alamat: "Jl. UMKM No. 123, Yogyakarta",
+    telepon: "0812-3456-7890",
+    email: "umkm@example.com",
+    mapsEmbedUrl: "https://www.google.com/maps/embed?...",
+  },
+};
+
+export default dataUMKM;
